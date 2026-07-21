d82d4e48a8fefa5da8371ec2fbddfcc2c8a09096
Hassaan <hassankhalids2007@gmail.com>
2026-07-21 23:01:36 +0530

    nonstaticmethod

--- nonstaticmethod.java
+++ nonstaticmethod.java
@@ -1,4 +1,4 @@
-public class my{
+/*public class my{
     void meow(){
         System.out.print("Meow");
     }
@@ -8,5 +8,28 @@ class nonstaticmethod{
     public static void main(String a[]){
         my n = new my();
         n.meow();
+    }
+}*/
+
+//not necessary that you have to make object in another class, you can make it within the class too
+
+public class nonstaticmethod{
+    String name;
+    String course;
+    String program;
+
+    void getinfo(){
+        name = "alex";
+        course = "bca";
+        program = "java";
+        System.out.println("Name is: " + name);
+        System.out.println("Course is: " + course);
+        System.out.println("Program is: " + program);
+    }
+
+
+    public static void main(String a[]){
+        nonstaticmethod myobject = new nonstaticmethod();
+        myobject.getinfo();
     }
 }
